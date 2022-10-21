c160315b4712e95f913e72e41c7581b593979722
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-10-21 15:54:33 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 let heartElement = document.getElementById('heart')
 let dataElement = document.getElementById('data')
+let connectBtnElement = document.getElementById('connectButton')
+let hrBtnElement = document.getElementById('hrButton')
 
 if (navigator.bluetooth === undefined) {
-  dataElement.textContent = 'Web bluetooth is off now, pls turn on'
+  alert('Web bluetooth is off now, pls turn on')
 }
 else {
-  let buttonElement = document.getElementById('connectButton')
-  buttonElement.style.cursor = 'pointer'
+  connectBtnElement.style.cursor = 'pointer'
 
-  let handleCharacteristicValueChanged = (event) => {
-    // console.log(event.target.value)
-    heartElement.hidden = false
-    let heartrate = event.target.value.getUint8(1)
-    dataElement.textContent = heartrate + " BPM"
-  }
+  let server = null
 
-  let onClickEvent = (event) => {
+  let onConnectBtnClick = (event) => {
     // alert('clicked')
-
-    // (https://web.dev/bluetooth/)
-    // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
     // navigator.bluetooth.requestDevice({ acceptAllDevices: true })  // --> BluetoothDevice {id: 'MCMj57ib5biFKGrDvYLlEg==', name: 'Polar Sense B5E5C221', gatt: BluetoothRemoteGATTServer, ongattserverdisconnected: null}
-    navigator.bluetooth.requestDevice({ filters: [{ name: 'Polar Sense B5E5C221' }] })
+    navigator.bluetooth.requestDevice({
+      filters: [{ name: 'Polar Sense B5E5C221' }],
+      // optionalServices: ["battery_service"],
+    })
       // .then(devices => console.log(devices))
-      .then(device => device.gatt.connect())  // conect to the server
-      .then(server => server.getPrimaryService('heart_rate'))  // get the service
-      .then(service => service.getCharacteristic('heart_rate_measurement')) // get the characteristics
-
-      .then(characteristic => characteristic.startNotifications())  // start listenning
-      .then(characteristic => {
-        characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged)  // add event listener
-      })      
+      .then(device => {  // conect to the server
+        server = device.gatt.connect()
+        return server
+      })
       .catch(error => { console.error(error) })
   }
-  buttonElement.addEventListener('click', onClickEvent)
+  connectBtnElement.addEventListener('click', onConnectBtnClick)
+
+  let onHrBtnClick = (event) => {
+    if (server === null) {
+      alert('Pls connect device first')
+    } else {
+      let handleCharacteristicValueChanged = (event) => {
+        // console.log(event.target.value)
+        heartElement.hidden = false
+        let heartrate = event.target.value.getUint8(1)
+        dataElement.textContent = heartrate + " BPM"
+      }
+      server
+        .then(server => server.getPrimaryService('heart_rate'))  // get the service
+        .then(service => service.getCharacteristic('heart_rate_measurement')) // get the characteristics
+  
+        .then(characteristic => characteristic.startNotifications())  // start listenning
+        .then(characteristic => {
+          characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged)  // add event listener
+        })      
+        .catch(error => console.error(error))
+    }
+  }
+  hrBtnElement.addEventListener('click', onHrBtnClick)
 }
 
-// https://www.youtube.com/watch?v=XDc5HUVMI5U
-// https://www.youtube.com/watch?v=ZIZqmXfrRLI
