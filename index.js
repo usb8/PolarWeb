7d917daa102340a7a4c526e8a181bf85cbd9be62
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-10-21 12:41:53 +0300

    scan & pair to sensor

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ else {
   buttonElement.style.cursor = 'pointer'
 
   let onClickEvent = (event) => {
-    alert('clicked')
+    // alert('clicked')
+
+    // (https://web.dev/bluetooth/)
+    // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
+    // navigator.bluetooth.requestDevice({ acceptAllDevices: true })  // --> BluetoothDevice {id: 'MCMj57ib5biFKGrDvYLlEg==', name: 'Polar Sense B5E5C221', gatt: BluetoothRemoteGATTServer, ongattserverdisconnected: null}
+    navigator.bluetooth.requestDevice({ filters: [{ name: 'Polar Sense B5E5C221' }] })
+      .then(devices => console.log(devices))
+      .catch(error => { console.error(error) })
   }
   buttonElement.addEventListener('click', onClickEvent)
 }
