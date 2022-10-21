ff4c8e4c705967272a5b7e18c03b7983dcab892f
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-10-21 13:15:46 +0300

    show: heart rate

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ else {
   let buttonElement = document.getElementById('connectButton')
   buttonElement.style.cursor = 'pointer'
 
+  let handleCharacteristicValueChanged = (event) => {
+    // console.log(event.target.value)
+    heartElement.hidden = false
+    let heartrate = event.target.value.getUint8(1)
+    dataElement.textContent = heartrate + " BPM"
+  }
+
   let onClickEvent = (event) => {
     // alert('clicked')
 
@@ -15,8 +22,19 @@ else {
     // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
     // navigator.bluetooth.requestDevice({ acceptAllDevices: true })  // --> BluetoothDevice {id: 'MCMj57ib5biFKGrDvYLlEg==', name: 'Polar Sense B5E5C221', gatt: BluetoothRemoteGATTServer, ongattserverdisconnected: null}
     navigator.bluetooth.requestDevice({ filters: [{ name: 'Polar Sense B5E5C221' }] })
-      .then(devices => console.log(devices))
+      // .then(devices => console.log(devices))
+      .then(device => device.gatt.connect())  // conect to the server
+      .then(server => server.getPrimaryService('heart_rate'))  // get the service
+      .then(service => service.getCharacteristic('heart_rate_measurement')) // get the characteristics
+
+      .then(characteristic => characteristic.startNotifications())  // start listenning
+      .then(characteristic => {
+        characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged)  // add event listener
+      })      
       .catch(error => { console.error(error) })
   }
   buttonElement.addEventListener('click', onClickEvent)
 }
+
+// https://www.youtube.com/watch?v=XDc5HUVMI5U
+// https://www.youtube.com/watch?v=ZIZqmXfrRLI
